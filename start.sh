cc2dde1618daf5a97c070cdc2d91a87b6881cdf4
jaylee0621 <1521685453@qq.com>
2025-03-09 20:44:49 +0800

    更新暂存

--- start.sh
+++ start.sh
@@ -1,8 +1,17 @@
-source ~/rm2025_hzu_sentry_ws/install/setup.bash && sleep 3 && \
-nohup ros2 launch bot_navigation2 test_all.launch.py > /dev/null 2>&1 & echo 1 && sleep 3 && \
-nohup ros2 launch bot_navigation2 navigation2.launch.py use_sim_time:=False slam:=False map:=/home/morefine/rm2025_hzu_sentry_ws/slam_map.yaml > /dev/null 2>&1 & echo 3 && sleep 3 && \
-nohup ros2 run map_tools nav2_to_goal > /dev/null 2>&1 & echo 4 && sleep 3 && \
-nohup ros2 run map_tools cmd_vel2serial.py > /dev/null 2>&1 & echo 5 && sleep 3 && \
-nohup ros2 run map_tools odo > /dev/null 2>&1 & echo 2 && sleep 3 && \
-nohup ros2 run map_tools serial_port > /dev/null 2>&1 & echo 6 && sleep 3 && \
-nohup ros2 run map_tools clear_cost_map > /dev/null 2>&1 & echo 7
+#!/bin/bash
+source /opt/ros/humble/setup.bash
+source /home/morefine/rm2025_hzu_sentry_ws/install/setup.bash
+
+# 启动所有进程
+ros2 launch bot_navigation2 test_all.launch.py &
+ros2 launch bot_navigation2 navigation2.launch.py use_sim_time:=False slam:=False map:=/home/morefine/rm2025_hzu_sentry_ws/slam_map.yaml &
+ros2 run map_tools nav2_to_goal &
+ros2 run map_tools cmd_vel2serial.py &
+ros2 run map_tools odo &
+ros2 run map_tools serial_port &
+ros2 run map_tools clear_cost_map &
+
+# 循环保持脚本运行
+while true; do
+    sleep 60
+done
